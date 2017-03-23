235b794403a3c59b6289f0b772c0bdcdc8a3ab06
alonso193 <alonso.espinoza193@gmail.com>
2017-03-23 12:39:42 -0600

    pidiendo dimensiones

--- matrix.cpp
+++ matrix.cpp
@@ -23,14 +23,17 @@ void clear_Screen(int rows){
   for (i = 0; i < rows; i++) {
     cout<<endl;
   }
-  
+
 }
 
 int main(int argc, char* argv[])
 {
+    int rows_param = atoi(argv[1]);
+    //int colums_param = stoi(argv[2]);
     int rows = get_dimention(0);
     int colums = get_dimention(1);
+    clear_Screen(rows);
     char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890|°¬!'?¿¡+*{[]}^`-.,;:_";
-    print_display(characters);
+    //print_display(characters);
     return 0;
 }
