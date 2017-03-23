be7affada171dc76b529cd80bb49dd4fbbdd6c6c
gnoma23 <arnaldochacon1996@gmail.com>
2017-03-23 11:53:24 -0600

    función obtener dimensión

--- matrix.cpp
+++ matrix.cpp
@@ -2,7 +2,17 @@
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
+#include <sys/ioctl.h>
 using namespace std;
+
+
+int get_dim(int pos)
+{
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+    int vec[2]= {w.ws_row,w.ws_col};
+    return vec[pos];
+}
 
 int main(int argc, char* argv[])
 {
