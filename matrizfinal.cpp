2b722dff0adad970b1aaf235ed92eb1839932eaa
alonso193 <alonso.espinoza193@gmail.com>
2017-03-25 10:49:14 -0600

    optimizando el main. Se sacaron los ciclos a una función printScreen

--- matrizfinal.cpp
+++ matrizfinal.cpp
@@ -11,6 +11,63 @@ int columns_param;
 int rows;
 int columns;
 
+//functions used
+void wait(int time);
+int get_dimention(int pos);
+void printScreen();
+void clear_Screen();
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    rows_param = atoi(argv[1]);
+    columns_param = atoi(argv[2]);
+    rows = get_dimention(0);
+    columns = get_dimention(1);
+    clear_Screen();
+    printScreen();
+    return 0;
+}
+
+void printScreen() {
+
+    int vector_randomize[columns];
+    char vector[columns];
+    int lagrima;
+
+    for (int n = 0; n < 20; n++)
+    {
+        lagrima = (rand() % 10 + 6);
+        for (int i = 0; i < columns; ++i)
+        {
+            vector_randomize[i] = rand()%46;
+        }
+        for (int k = 0; k < lagrima; ++k)
+        {
+            for (int j = 0; j < columns; ++j)
+            {
+                if (vector_randomize[j] < (rand()%6+5))
+                {
+                    vector[j] = rand() % 94 + 33;
+                }
+                else
+                {
+                    vector[j] = 32;
+                }
+            }
+            for (int j = 0; j < columns; ++j)
+            {
+                wait(700);
+                cout << "\e[92m" << vector[j];
+                if (j == (columns - 1))
+                {
+                    cout << "\n";
+                }
+            }
+        }
+    }
+}
+
 void wait(int time)
 {
    usleep(time);
@@ -24,68 +81,7 @@ int get_dimention(int pos)
     return vec[pos];
 }
 
-void clear_Screen(){
-  int i;
-  for (i = 0; i < rows; i++) {
-    cout<<"\r\n\r";
-  }
-  cout << "\r";
-}
-
-int main(int argc, char* argv[])
+void clear_Screen()
 {
-    srand(time(NULL));
-    rows_param = atoi(argv[1]);
-    columns_param = atoi(argv[2]);
-    rows = get_dimention(0);
-    columns = get_dimention(1);
-    // clear_Screen();
-
-    int vector_randomize[columns];
-    char vector[columns];
-
-    int lagrima = rand() % 19 + 6;
-
-    for (int n = 0; n < 10; n++) 
-    {
-    //clear_Screen();
-
-        int vector_randomize[columns];
-        char vector[columns];
-        int lagrima = rand() % 10 + 6;
-        for (int n = 0; n < 20; n++)
-        {
-            lagrima = rand() % 19 + 6;
-            for (int i = 0; i < columns; ++i)
-            {
-                vector_randomize[i] = rand()%46;
-            }
-            for (int k = 0; k < lagrima; ++k)
-            {
-
-                for (int j = 0; j < columns; ++j)
-                {
-                    if (vector_randomize[j] < (rand()%6+5))
-                    {
-                        vector[j] = rand() % 94 + 33;
-                    }
-                    else
-                    {
-                        vector[j] = 32;
-                    }
-                }
-                for (int j = 0; j < columns; ++j)
-                {
-                    wait(450);
-                    cout << "\e[92m" << vector[j];
-                    if (j == (columns - 1))
-                    {
-                        cout << "\n";
-                    }
-                }
-
-            }
-        }
-    }
-return 0;
+    system("clear");
 }
